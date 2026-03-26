f241c3eeeddbef5a2949d577a33870b7b5ad9e8e
jasper <Jasper.v.kalsbeek@gmail.com>
2026-03-26 16:53:19 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   img/Spik en Span.png
    	new file:   public/Employees.html
    	new file:   public/Login.html
    	new file:   public/Orders.html
    	modified:   public/index.php
    	new file:   public/style.css

--- public/index.php
+++ public/index.php
@@ -7,9 +7,34 @@ include "../includes/db.php"
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>test</title>
+    <title>Spik en Span Tickets bestellen</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    hello world
+
+
+<h1>Tickets bestellen?</h1>
+<div class="PrimaryContainer">
+    <div class="bestellen">
+        <p>Kaarten voor <strong>Spik en Span</strong> op <strong> 15 of 16 augustus</strong> op kasteel Limbricht.</p>
+        <a href="#" class="btn-primary"><button>Snel tickets bestellen</button></a>
+    </div>
+</div>
+<br>
+<div class="SecondaryContainer">
+<h2>Al een kaartje gekocht?</h2>
+    <div class="inloggen" class="btn-secondary">
+        <a href="#"> <button>Inloggen</button></a>
+        <p class="klein">Om je tickets te bekijken, downloaden of printen.</p>
+    </div>
+
+<h2>Medewerker?</h2>
+    <div class="medewerker">
+        <a href="#" class="btn-secondary">
+            <button>Inloggen medewerker</button>
+        </a>
+    </div>
+</div>
+
 </body>
 </html>

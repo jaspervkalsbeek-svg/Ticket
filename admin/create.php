4d524e8ea8170acde9b8e06eeb396f96bdd9b365
jasper van kalsbeek <jasper.v.kalsbeek@gmail.com>
2026-04-01 13:38:59 +0200

    ¿

--- admin/create.php
+++ admin/create.php
@@ -1,7 +1,7 @@
     <?php
     include ‘db.php’; // Include the database connection
 
-    if (isset($_POST[‘submit’])) {
+    if (isset($_POST['submit'])) {
     $email = $_POST[‘email’];
     $leeftijd = $_POST[‘leeftijd’];
     $fname = $_POST[‘fname’];

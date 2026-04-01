55b04e4ae9dd351c65e2d5ea8d873b55e23c50d9
bloomylumi <noelrileygames@gmail.com>
2026-04-01 13:11:21 +0200

    create.php test

---
+++ admin/create.php
@@ -0,0 +1,27 @@
+    <?php
+    include ‘db.php’; // Include the database connection
+
+    if (isset($_POST[‘submit’])) {
+    $name = $_POST[‘name’];
+    $email = $_POST[‘email’];
+    $phone = $_POST[‘phone’];
+
+    $sql = “INSERT INTO users (email, leeftijd, fname, lname, herkomst, ticket_id) VALUES (‘$email’, ‘$leeftijd’, ‘$fname’, ‘$lname’, ‘$herkomst’, ‘$ticketid’)”;
+
+    if ($conn->query($sql) === TRUE) {
+    echo “New record created successfully”;
+    } else {
+    echo “Error: “ . $sql . “<br>” . $conn->error;
+    }
+    }
+    ?>
+
+    <form action=”create.php” method=”post”>
+    Email: <input type=”email” name=”email”><br>
+    Leeftijd: <input type=”text” name=”leeftijd”><br>
+    First Name: <input type=”text” name=”Firstname”><br>
+    Last Name: <input type=”text” name=”Lastname”><br>
+    Herkomst: <input type=”text” name=”Herkomst”><br>
+    Ticket ID: <input type=”text” name=”TicketID”><br>
+    <input type=”submit” name=”submit” value=”Create User”>
+    </form>

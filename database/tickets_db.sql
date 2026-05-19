fe65b8daa0e024ef6a83085eb5deea357d6ee8b0
jasper van kalsbeek <jasper.v.kalsbeek@gmail.com>
2026-05-19 18:59:12 +0200

    renamed:    database/tickets_db (3).sql -> database/tickets_db.sql 	modified:   obj/Debug/net10.0/Ticket.AssemblyInfo.cs 	modified:   obj/Debug/net10.0/Ticket.AssemblyInfoInputs.cache

--- database/tickets_db.sql
+++ database/tickets_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 17 mei 2026 om 01:25
+-- Gegenereerd op: 19 mei 2026 om 18:58
 -- Serverversie: 10.4.32-MariaDB
 -- PHP-versie: 8.2.12
 
@@ -55,24 +55,26 @@ INSERT INTO `coupon_tb` (`id`, `name`, `korting_euro`, `korting_%`, `event_id`, 
 CREATE TABLE `events` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `discription` text NOT NULL,
+  `description` text NOT NULL,
   `start_date` datetime NOT NULL,
   `end_date` datetime NOT NULL,
-  `location` varchar(255) NOT NULL
+  `location` varchar(255) NOT NULL,
+  `description_li` varchar(255) NOT NULL,
+  `name_li` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `events`
 --
 
-INSERT INTO `events` (`id`, `name`, `discription`, `start_date`, `end_date`, `location`) VALUES
-(1, 'Spik & Span XXL 2026 vrijdag', 'Spik & Span XXL Op 21 augustus 2026 feesten we samen met Spik & Span én gastartiesten in de unieke setting van Kasteel Limbricht', '2026-08-21 18:00:00', '2026-08-21 23:00:00', 'Landgoed Kasteel Limbricht, Limbricht'),
-(2, 'Spik & Span XXL 2026 zaterdag', 'Spik & Span XXL Op 22 augustus 2026 feesten we samen met Spik & Span én gastartiesten in de unieke setting van Kasteel Limbricht', '2026-08-22 18:00:00', '2026-08-22 23:00:00', 'Landgoed Kasteel Limbricht, Limbricht'),
-(3, 'spik en test', 'zeg maar eens wat', '2026-05-13 15:13:00', '2026-05-14 15:13:00', 'op den kamp Landgraaf'),
-(4, 'test en span 2028', 'zeg nog eens wat maar ja dat ja', '2026-05-13 15:15:00', '2026-05-15 15:15:00', 'kerkrade'),
-(5, 'test en span 2028', 'zeg nog eens wat maar ja dat ja', '2026-05-13 15:15:00', '2026-05-15 15:15:00', 'kerkrade'),
-(6, 'test en span 2028', 'zeg nog eens wat maar ja dat ja', '2026-05-13 15:15:00', '2026-05-15 15:15:00', 'kerkrade'),
-(7, 'test en span 2028', 'zeg nog eens wat maar ja dat ja', '2026-05-13 15:15:00', '2026-05-15 15:15:00', 'kerkrade');
+INSERT INTO `events` (`id`, `name`, `description`, `start_date`, `end_date`, `location`, `description_li`, `name_li`) VALUES
+(1, 'Spik & Span XXL 2026 vrijdag', 'Spik & Span XXL Op 21 augustus 2026 feesten we samen met Spik & Span én gastartiesten in de unieke setting van Kasteel Limbricht', '2026-08-21 18:00:00', '2026-08-21 23:00:00', 'Landgoed Kasteel Limbricht, Limbricht', 'Spik & Span XXL Op 21 augustus 2026 viere we same', 'Spik & Span XXL 2026 Vriejdaag'),
+(2, 'Spik & Span XXL 2026 zaterdag', 'Spik & Span XXL Op 22 augustus 2026 feesten we samen met Spik & Span én gastartiesten in de unieke setting van Kasteel Limbricht', '2026-08-22 18:00:00', '2026-08-22 23:00:00', 'Landgoed Kasteel Limbricht, Limbricht', 'Spik & Span XXL Op 22 augustus 2026 viere we same', 'Spik & Span XXL 2026 Zaterdig'),
+(3, 'spik en test', 'zeg maar eens wat', '2026-05-13 15:13:00', '2026-05-14 15:13:00', 'op den kamp Landgraaf', 'zeg mer ins wat', 'spik en test'),
+(4, 'test en span 2028', 'zeg nog eens wat maar ja dat ja', '2026-05-13 15:15:00', '2026-05-15 15:15:00', 'kerkrade', 'zeg nog ins wat mer ja dat ja', 'test en span 2028'),
+(5, 'test en span 2028', 'zeg nog eens wat maar ja dat ja', '2026-05-13 15:15:00', '2026-05-15 15:15:00', 'kerkrade', 'zeg nog ins wat mer ja dat ja', 'test en span 2028'),
+(6, 'test en span 2028', 'zeg nog eens wat maar ja dat ja', '2026-05-13 15:15:00', '2026-05-15 15:15:00', 'kerkrade', 'zeg nog ins wat mer ja dat ja', 'test en span 2028'),
+(7, 'test en span 2028', 'zeg nog eens wat maar ja dat ja', '2026-05-13 15:15:00', '2026-05-15 15:15:00', 'kerkrade', 'zeg nog ins wat mer ja dat ja', 'test en span 2028');
 
 -- --------------------------------------------------------
 
@@ -89,17 +91,24 @@ CREATE TABLE `orders` (
   `geboortedatum` date DEFAULT NULL,
   `event_id` int(11) DEFAULT NULL,
   `total_price` decimal(10,2) DEFAULT NULL,
-  `created_at` datetime DEFAULT current_timestamp()
+  `created_at` datetime DEFAULT current_timestamp(),
+  `herkomst` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `orders`
 --
 
-INSERT INTO `orders` (`id`, `Fname`, `Lname`, `email`, `Aanhef`, `geboortedatum`, `event_id`, `total_price`, `created_at`) VALUES
-(1, 'test', 'test', 'jasper.v.kalsbeek@gmail.com', 'Man', '0000-00-00', 1, 80.00, '2026-05-12 16:25:42'),
-(2, 'test', 'test', 'jasper.v.kalsbeek@gmail.com', 'Man', '0000-00-00', 2, 55.00, '2026-05-12 18:04:06'),
-(3, 'jasper', 'van kalsbeek', 'jasper.v.kalsbeek@gmail.com', 'Man', '0000-00-00', 2, 40.00, '2026-05-13 08:24:03');
+INSERT INTO `orders` (`id`, `Fname`, `Lname`, `email`, `Aanhef`, `geboortedatum`, `event_id`, `total_price`, `created_at`, `herkomst`) VALUES
+(1, 'test', 'test', 'jasper.v.kalsbeek@gmail.com', 'Man', '0000-00-00', 1, 80.00, '2026-05-12 16:25:42', 'Groningen'),
+(2, 'test', 'test', 'jasper.v.kalsbeek@gmail.com', 'Man', '0000-00-00', 2, 55.00, '2026-05-12 18:04:06', 'Friesland'),
+(3, 'jasper', 'van kalsbeek', 'jasper.v.kalsbeek@gmail.com', 'Man', '0000-00-00', 2, 40.00, '2026-05-13 08:24:03', 'Utrecht'),
+(4, 'jasper', 'asdf', 'jasper.v.kalsbeek@gmail.com', 'Man', NULL, 3, 10.00, '2026-05-19 11:00:37', 'Zuid-Holland'),
+(5, 'a;kdjf', 'asdfjk', 'jasper.v.kalsbeek@gmail.com', 'Man', NULL, 3, 10.00, '2026-05-19 11:07:05', 'Zeeland'),
+(6, 'je', 'moeder', 'jasper.v.kalsbeek@gmail.com', 'None', NULL, 4, 50.00, '2026-05-19 11:54:52', 'Utrecht'),
+(7, 'je', 'moeder', 'jasper.v.kalsbeek@gmail.com', 'None', NULL, 4, 50.00, '2026-05-19 11:54:54', 'Noord-Holland'),
+(8, 'je', 'moeder', 'jasper.v.kalsbeek@gmail.com', 'None', NULL, 4, 50.00, '2026-05-19 11:55:28', 'Gelderland'),
+(9, 'jasper', 'van kalsbeek', 'jasper.v.kalsbeek@gmail.com', 'Man', NULL, 3, 10.00, '2026-05-19 16:13:05', 'Limburg');
 
 -- --------------------------------------------------------
 
@@ -132,7 +141,16 @@ INSERT INTO `tickets_tb` (`id`, `email`, `ticket_id`, `Fname`, `Lname`, `date`, 
 (5, 'jasper.v.kalsbeek@gmail.com', '4F8EE6EB9236', 'test', 'test', '2026-05-12', 0, 0, 2, 4),
 (6, 'jasper.v.kalsbeek@gmail.com', '6E3AAD3B6E0B', 'test', 'test', '2026-05-12', 0, 0, 2, 5),
 (7, 'jasper.v.kalsbeek@gmail.com', '110E76DAE30A', 'jasper', 'van kalsbeek', '2026-05-13', 0, 0, 3, 5),
-(8, 'jasper.v.kalsbeek@gmail.com', '97B450703817', 'jasper', 'van kalsbeek', '2026-05-13', 0, 0, 3, 5);
+(8, 'jasper.v.kalsbeek@gmail.com', '97B450703817', 'jasper', 'van kalsbeek', '2026-05-13', 0, 0, 3, 5),
+(9, 'jasper.v.kalsbeek@gmail.com', '0EA9BE6B9700', 'jasper', 'asdf', '2026-05-19', 0, 0, 4, 13),
+(10, 'jasper.v.kalsbeek@gmail.com', '6AD69F46DB3E', 'a;kdjf', 'asdfjk', '2026-05-19', 0, 0, 5, 13),
+(11, 'jasper.v.kalsbeek@gmail.com', '5CA9C99517BF', 'je', 'moeder', '2026-05-19', 0, 0, 6, 8),
+(12, 'jasper.v.kalsbeek@gmail.com', '6B4CFC556F8F', 'je', 'moeder', '2026-05-19', 0, 0, 6, 9),
+(13, 'jasper.v.kalsbeek@gmail.com', '8E2E63B96039', 'je', 'moeder', '2026-05-19', 0, 0, 7, 8),
+(14, 'jasper.v.kalsbeek@gmail.com', '2A4A4629C9AC', 'je', 'moeder', '2026-05-19', 0, 0, 7, 9),
+(15, 'jasper.v.kalsbeek@gmail.com', '48D61ED66A33', 'je', 'moeder', '2026-05-19', 0, 0, 8, 8),
+(16, 'jasper.v.kalsbeek@gmail.com', '20DC71763D03', 'je', 'moeder', '2026-05-19', 0, 0, 8, 9),
+(17, 'jasper.v.kalsbeek@gmail.com', '819A08061DD7', 'jasper', 'van kalsbeek', '2026-05-19', 0, 0, 9, 13);
 
 -- --------------------------------------------------------
 
@@ -228,13 +246,13 @@ ALTER TABLE `events`
 -- AUTO_INCREMENT voor een tabel `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT voor een tabel `tickets_tb`
 --
 ALTER TABLE `tickets_tb`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT voor een tabel `ticket_type_tb`
